e0bdb5180864b0b86e2f3d665e95907032117741
panday801 <kumarpanday683@gmail.com>
2022-06-19 19:15:22 +0530

    signup and login_aman

---
+++ scripts/forget.js
@@ -0,0 +1,15 @@
+function forgeta(){
+  return`
+  
+  <div id="emaai">
+  <div class="field">
+      <input type="number" name="otp" id="otp" placeholder="Enter OTP">
+      <label for="otp">O.T.P</label>
+    </div>
+  </div>
+  
+  
+  
+  `
+}
+export default forgeta
